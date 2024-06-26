4b6177f7c270740cecebe4ed72fe42d6c517b174
Joel Longanecker <joel.longanecker@gmail.com>
2024-06-26 14:56:12 -0500

    updates

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t ultimate-homebrew-gba -f GBA.Dockerfile .
-docker build -t ultimate-homebrew-ps1 -f PS1.Dockerfile .
-docker build -t ultimate-homebrew-ps2 -f PS2.Dockerfile .
-docker build -t ultimate-homebrew-ps3 -f PS3.Dockerfile .
-docker build -t ultimate-homebrew-psp -f PSP.Dockerfile .
+docker build -t longjoelhome/ultimate-homebrew-gba -f GBA.Dockerfile . && docker push longjoelhome/ultimate-homebrew-gba
+docker build -t longjoelhome/ultimate-homebrew-ps1 -f PS1.Dockerfile . && docker push longjoelhome/ultimate-homebrew-ps1
+docker build -t longjoelhome/ultimate-homebrew-ps2 -f PS2.Dockerfile . && docker push longjoelhome/ultimate-homebrew-ps2
+docker build -t longjoelhome/ultimate-homebrew-ps3 -f PS3.Dockerfile . && docker push longjoelhome/ultimate-homebrew-ps3
+docker build -t longjoelhome/ultimate-homebrew-psp -f PSP.Dockerfile . && docker push longjoelhome/ultimate-homebrew-psp
